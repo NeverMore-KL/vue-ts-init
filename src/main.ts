fe3c0d480cfff99912bef8720efe82b47faed729
1 <452287388@qq.com>
2018-07-11 20:30:36 +0800

    tianjia ele ui

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ import store from "./store/index";
 // import { stringify } from "qs";
 import "./utils/polyfill";
 import { Button, Message } from "element-ui";
+// 自定义主题颜色
+import "./style/el-theme.scss";
 Vue.use(Button);
 //@ts-ignore
 Vue.use(Message);
@@ -30,65 +32,3 @@ const V = new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
-//涉及到ios分享才需要如此
-/**
- function jump_router_ios(_location: any, isReplace: boolean): void {
-  // 与window.location做去区分
-  const type = typeof _location;
-  let url: string = "";
-  if (type === "string") {
-    window.location.href = _location;
-  } else {
-    // 详细规则:https://router.vuejs.org/zh/guide/essentials/navigation.html
-    if ("path" in _location) {
-      if ("params" in _location) {
-        throw new Error("`path` 不能使用 `params`");
-      } else if ("query" in _location) {
-        url = _location.path + "?" + stringify(_location.query);
-      } else {
-        url = _location.path;
-      }
-      isReplace ? window.location.replace(url) : (window.location.href = url);
-    }
-  }
-}
- */
-
-/**
- * example
- * 字符串模式(与官网一致) : router._push('home')
- * query模式(与官网一致) : router.push({ path: 'register', query: { plan: 'private' }})
- * params模式(无法使用命名路由) : router.push({ path: `/user/${userId}` })
- */
-// @ts-ignore
-/*
-V.$router._push = (location: any, onComplete: Function, onAbort: Function) => {
-  //暂不支持命名模式
-  if ("name" in location) {
-    throw new Error("暂不支持通过命名路由跳转,请使用path或者字符串模式");
-  }
-  if (getAppVersion() === "ios") {
-    jump_router_ios(location, false);
-  } else if (getAppVersion() === "android") {
-    V.$router.push(location, onComplete, onAbort);
-  }
-};
-// _replace方法与_push使用一致
-// @ts-ignore
-V.$router._replace = (
-  location: any,
-  onComplete: Function,
-  onAbort: Function
-) => {
-  //暂不支持命名模式
-  if ("name" in location) {
-    throw new Error("暂不支持通过命名路由跳转,请使用path或者字符串模式");
-  }
-  if (getAppVersion() === "ios") {
-    jump_router_ios(location, true);
-  } else if (getAppVersion() === "android") {
-    V.$router.replace(location, onComplete, onAbort);
-  }
-};
-*/
